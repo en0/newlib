2af95aa7deb434c4d59aca2a9f5540d366afb558
Ian Laird <irlaird@gmail.com>
2017-08-01 13:18:51 -0600

    Moved environment variables outside of the build.sh script so that the script can be reused.

--- build.sh
+++ build.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env bash
 
-which i686-rpos-cc >/dev/null
-if [ $? != 0 ]; then
-    echo OS Toolchain is not in path. Cannot build.
-    exit 1
-fi
+#which i686-rpos-cc >/dev/null
+#if [ $? != 0 ]; then
+    #echo OS Toolchain is not in path. Cannot build.
+    #exit 1
+#fi
+#DESTDIR="$(readlink -f $PWD/../../../)"
+##../configure --prefix=/usr --target=i686-rpos
 
-DESTDIR="$(readlink -f $PWD/../../../)"
+abort() {
+    MESSAGE=$1
+    EXITCODE=$2
+    if [[ -z "$EXITCODE" ]]
+    then
+        EXITCODE=1
+    fi
+
+    printf "ERROR: %s\n" "${MESSAGE}" 1>&2
+    exit $EXITCODE
+}
+
+require_env() {
+    [[ -z "$2" ]] && \
+        abort "\$$1 is not set. Please source the desired environment." 1
+}
+
+require_env "SYSROOT" "${SYSROOT}"
+require_env "PREFIX" "${PREFIX}"
+require_env "TARGET" "${TARGET}"
+require_env "PATH" "${PATH}"
 
 rm -rf newlib-build
 mkdir -p newlib-build
 cd newlib-build
 
-../configure --prefix=/usr --target=i686-rpos
-make all
-make DESTDIR=$DESTDIR install
-
-cd -
+../configure \
+    --prefix=${PREFIX} \
+    --with-build-sysroot=${SYSROOT} \
+    --target=${TARGET} && make all && make install
